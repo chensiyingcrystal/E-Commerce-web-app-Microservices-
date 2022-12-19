09599336ed7c7e9ee86d1b29ab67e7695b10646e
Siying Chen <chensiyingcrystal@gmail.com>
2022-12-19 09:04:30 +0800

    Update index.ts

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import { json } from 'body-parser';
 
 const app = express();
+//use json middleware to parse json from incoming request
+//without json, we cannot use req.body
 app.use(json());
 
 app.listen(3000, () => {
