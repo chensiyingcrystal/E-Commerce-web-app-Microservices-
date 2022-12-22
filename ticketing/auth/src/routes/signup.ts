ecb4091da7047bd789a71afd1df2ce5af2d20ed0
Siying Chen <chensiyingcrystal@gmail.com>
2022-12-23 05:42:26 +0800

    update

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -12,15 +13,27 @@ router.post('/api/users/signup', [
 ], 
 async (req: Request, res: Response) => {
     const errors = validationResult(req);
-
+    //initial request validation
     if (!errors.isEmpty()) {
         throw new RequestValidationError(errors.array());
     }
 
-    console.log('Creating a user...');
-    throw new DatabaseConnectionError();
+    //see if someone has registered this email
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email});
 
-    res.send({});
+    if (existingUser) {
+        throw new BadRequestError('Email in use');
+    }
+    //building user
+    const user = User.build({ 
+        email, password
+    });
+    //save user to db
+    await user.save();
+
+    res.status(201).send(user);
+
 
 });
 
