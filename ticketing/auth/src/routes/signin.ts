d461873d8eae57014df786c8cd69731fbe36e401
Siying Chen <chensiyingcrystal@gmail.com>
2022-12-24 11:10:52 +0800

    update

--- ticketing/auth/src/routes/signin.ts
+++ ticketing/auth/src/routes/signin.ts
@@ -1,8 +1,7 @@
 import express from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
+import { validateRequest } from '../models/validate-request';
 import { Request, Response } from 'express';
-
-import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
@@ -11,15 +10,10 @@ router.post('/api/users/signin',
     body('email').isEmail().withMessage('Email must be valid'), 
     body('password').trim().notEmpty().withMessage('You must supply a passoword')
 ], 
+validateRequest,
 (req: Request, res: Response) => {
-    const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-    }
-
-
-    res.send("Hi there new edition");
+    res.send("Hi there sign in...");
 });
 
 export { router as signinRouter };
--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
+import { validateRequest } from '../models/validate-request';
 import { User } from '../models/user';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
@@ -12,13 +12,8 @@ router.post('/api/users/signup', [
     body('password').trim().isLength({ min: 4, max: 20 })
     .withMessage('Password must be between 4 and 20 characters')
 ], 
+validateRequest,
 async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    //initial request validation
-    if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-    }
-
     //see if someone has registered this email
     const { email, password } = req.body;
     const existingUser = await User.findOne({ email});
