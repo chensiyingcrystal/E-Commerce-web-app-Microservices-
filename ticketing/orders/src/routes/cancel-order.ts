32a015c1fb25ff2d0fad5c58af255896e3794451
Siying Chen <chensiyingcrystal@gmail.com>
2023-01-06 20:32:33 +0800

    Update cancel-order.ts

--- ticketing/orders/src/routes/cancel-order.ts
+++ ticketing/orders/src/routes/cancel-order.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express';
+import { Order } from '../models/order';
+import { OrderStatus, requireAuth } from '@crystaltickets/common';
 
 const router = express.Router();
-
+//maybe delete method is not the best choice
+//we can use put as we only want to update the order's status to cancelled
 router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
     res.send({});
 });
