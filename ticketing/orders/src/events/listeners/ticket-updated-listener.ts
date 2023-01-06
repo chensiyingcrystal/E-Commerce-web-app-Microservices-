d03dd073f8a0dc8f45257360956f5a557228ebd9
Siying Chen <chensiyingcrystal@gmail.com>
2023-01-07 00:30:51 +0800

    Create ticket-updated-listener.ts

---
+++ ticketing/orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,24 @@
+import { Message } from "node-nats-streaming";
+import { Subjects, Listener, TicketUpdatedEvent } from "@crystaltickets/common";
+import { Ticket } from "../../models/ticket";
+import { queueGroupName } from "./queue-group-name";
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+    subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+    queueGroupName: string = queueGroupName;
+//first arg: data from inside our event, second arg: message from nats server(underlying property and built-in property)
+    async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
+        //find our ticket, update it using updated ticket
+        const ticket = await Ticket.findById(data.id);
+
+        if (!ticket) {
+            throw new Error('Ticket not found');
+        }
+
+        const { title, price } = data;
+        ticket.set({ title, price });
+        await ticket.save();
+
+        msg.ack();
+    }
+}
