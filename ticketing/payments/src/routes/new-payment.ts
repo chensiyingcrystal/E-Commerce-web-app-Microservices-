4449f2673834f93e6fdae5b26849ef76a694925b
Siying Chen <chensiyingcrystal@gmail.com>
2023-01-09 22:14:45 +0800

    update payment in ingress

--- ticketing/payments/src/routes/new-payment.ts
+++ ticketing/payments/src/routes/new-payment.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
-import { requireAuth, validateRequest, BadRequestError, NotFoundError } from '@crystaltickets/common';
+import { requireAuth, validateRequest, BadRequestError, NotFoundError, NotAuthroizedError, OrderStatus } from '@crystaltickets/common';
 import { Order } from '../models/order';
 
 const router = express.Router();
@@ -8,8 +8,24 @@ const router = express.Router();
 router.post('/api/payments', requireAuth, [
     body('token').not().isEmpty().withMessage('Token is required for payments'),
     body('orderId').not().isEmpty().withMessage('Order id is required')
-], (req: Request, res: Response) => {
-    res.send({ success: true });
+], validateRequest, async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+        throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+        throw new NotAuthroizedError();
+    }
+
+    if (order.status === OrderStatus.Cancelled) {
+        throw new BadRequestError('Order already cancelled');
+    }
+
+    res.status(201).send(order);
 
 });
 
