8beca00a94da300ca8f1204b5d5de69bd11ac702
Siying Chen <chensiyingcrystal@gmail.com>
2023-01-11 00:20:40 +0800

    update navigate to order show

--- ticketing/client/pages/tickets/[ticketId].js
+++ ticketing/client/pages/tickets/[ticketId].js
@@ -1,8 +1,29 @@
+import useRequest from '../../hooks/use-request';
+import Router from 'next/router';
+
 const TicketShow = ({ ticket }) => {
+    const { doRequest, errors } = useRequest({
+        url: '/api/orders',
+        method: 'post',
+        body: {
+            ticketId: ticket.id
+        },
+        onSuccess: (order) => {
+            //navigate to order show page
+            Router.push(
+                '/orders/[orderId]',  //generic url(same to directory path)
+                `/orders/${order.id}` //real url
+            )
+
+        }
+    });
+
     return (
         <div>
             <h1>{ticket.title}</h1>
             <h4>Price: {ticket.price}</h4>
+            {errors}
+            <button onClick={doRequest} className="btn btn-primary">Purchase</button>
         </div>
     );
 };
